dc9a15cb0307c50c4b8065d4110c74dc9f6e51bf
Yasya El Hakim <yasyaelhakim@gmail.com>
2023-09-30 04:51:28 +0700

    update translate.min.js and translate.js

--- src/scripts/translate.js
+++ src/scripts/translate.js
@@ -5,48 +5,41 @@ const textareaTranslation = document.getElementById('textarea_tools_language_tra
 const selectEngine = document.getElementById('select_tools_engine');
 const selectTranslateTo = document.getElementById('select_tools_language_translation');
 
-const updateUrlParams = async () => {
-    const textValue = textareaText.value.trim();
-    const engineValue = selectEngine.value;
-    const toValue = selectTranslateTo.value;
+if (textareaText && textareaTranslation && selectEngine && selectTranslateTo) {
+    const updateUrlParams = async () => {
+        // Check if textareaText is empty or blank
+        if (textareaText.value.trim() === '') {
+            // You can optionally set textareaTranslation to an empty string or some default value
+            textareaTranslation.value = '';
+            return; // Exit the function early
+        }
 
-    // Update the URL parameters
-    const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('materiaTools', 'translate');
-    urlParams.set('text', textValue);
-    urlParams.set('engine', engineValue);
-    urlParams.set('to', toValue);
-
-    // Update the URL without refreshing the page
-    window.history.replaceState({}, '', `?${urlParams.toString()}`);
-
-    try {
         const baseUrl = 'https://api-translate.azharimm.dev/translate';
         const url = new URL(baseUrl);
 
-        url.searchParams.set('engine', engineValue);
-        url.searchParams.set('to', toValue);
-        url.searchParams.set('text', textValue);
+        url.searchParams.set('engine', selectEngine.value);
+        url.searchParams.set('to', selectTranslateTo.value);
 
-        const response = await fetch(url.href);
+        url.searchParams.set('text', textareaText.value);
 
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+        try {
+            const response = await fetch(url.href);
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+
+            textareaTranslation.value = data.data.result;
+        } catch (error) {
+            snackbar('Fetch Error: ' + error, 3000);
         }
+    };
 
-        const data = await response.json();
-
-        textareaTranslation.value = data.data.result;
-    } catch (error) {
-        snackbar('Fetch Error: ' + error, 3000);
-    }
-};
-
-if (textareaText && textareaTranslation && selectEngine && selectTranslateTo) {
     selectEngine.addEventListener('change', updateUrlParams);
     selectTranslateTo.addEventListener('change', updateUrlParams);
     textareaText.addEventListener('input', updateUrlParams);
 
-    // Update URL parameters on initial page load
     updateUrlParams();
 }
