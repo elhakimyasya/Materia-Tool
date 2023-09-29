554c001d87ce4af363a0c7a1e1bd3af03715dfa9
Yasya El Hakim <yasyaelhakim@gmail.com>
2023-09-30 04:40:57 +0700

    update 4 files and create 1 file

--- src/scripts/translate.js
+++ src/scripts/translate.js
@@ -5,39 +5,44 @@ const textareaTranslation = document.getElementById('textarea_tools_language_tra
 const selectEngine = document.getElementById('select_tools_engine');
 const selectTranslateTo = document.getElementById('select_tools_language_translation');
 
-if (textareaText && textareaTranslation && selectEngine && selectTranslateTo) {
-    const updateUrlParams = async () => {
-        // Check if textareaText is empty or blank
-        if (textareaText.value.trim() === '') {
-            // You can optionally set textareaTranslation to an empty string or some default value
-            textareaTranslation.value = '';
-            return; // Exit the function early
-        }
+const updateUrlParams = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
 
-        const baseUrl = 'https://api-translate.azharimm.dev/translate';
-        const url = new URL(baseUrl);
+    if (urlParams.has('materiaTools') && urlParams.get('materiaTools') === 'translate') {
+        const textParam = urlParams.get('text');
+        const engineParam = urlParams.get('engine');
+        const toParam = urlParams.get('to');
 
-        url.searchParams.set('engine', selectEngine.value);
-        url.searchParams.set('to', selectTranslateTo.value);
+        if (textParam && engineParam && toParam) {
+            textareaText.value = textParam;
+            selectEngine.value = engineParam;
+            selectTranslateTo.value = toParam;
 
-        url.searchParams.set('text', textareaText.value);
+            try {
+                const baseUrl = 'https://api-translate.azharimm.dev/translate';
+                const url = new URL(baseUrl);
 
-        try {
-            // Fetch data asynchronously
-            const response = await fetch(url.href);
+                url.searchParams.set('engine', engineParam);
+                url.searchParams.set('to', toParam);
+                url.searchParams.set('text', textParam);
 
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
+                const response = await fetch(url.href);
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+
+                textareaTranslation.value = data.data.result;
+            } catch (error) {
+                snackbar('Fetch Error: ' + error, 3000);
             }
-
-            const data = await response.json();
-
-            textareaTranslation.value = data.data.result;
-        } catch (error) {
-            snackbar('Fetch Error: ' + error, 3000);
         }
-    };
+    }
+};
 
+if (textareaText && textareaTranslation && selectEngine && selectTranslateTo) {
     selectEngine.addEventListener('change', updateUrlParams);
     selectTranslateTo.addEventListener('change', updateUrlParams);
     textareaText.addEventListener('input', updateUrlParams);
